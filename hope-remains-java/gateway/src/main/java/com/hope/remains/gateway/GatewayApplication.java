671a19f1b1afc8be1174327f3370a96e6c2b0654
tangrong <rong_tang@egov-china.com>
2020-08-02 16:03:32 +0800

    网关初步搭建

--- hope-remains-java/gateway/src/main/java/com/hope/remains/gateway/GatewayApplication.java
+++ hope-remains-java/gateway/src/main/java/com/hope/remains/gateway/GatewayApplication.java
@@ -2,8 +2,15 @@ package com.hope.remains.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @description: 项目启动入口
+ * @author: tr
+ * @date: 2020/8/1
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
